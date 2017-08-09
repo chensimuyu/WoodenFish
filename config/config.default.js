d71df25ec7a583cdd39a0552a81b990d53263dac
qiang.yu <qiang.yu@ucarinc.com>
2017-08-09 23:40:52 +0800

    添加egg框架

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_csmy1231';
+
+  // add your config here
+
+  return config;
+};
